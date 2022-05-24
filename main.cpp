a8d222824f880785ea2e5326446730865a6a4861
Marija <marijaristevska02@gmail.com>
2022-05-24 11:58:43 +0200

    Inicijalizacija na proektot. Kreirani dve funkcii rankCard() koja sluzi za formiranje na vrednostite i suitCard() kojoa sluzi za formiranje na znacite

---
+++ main.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <ctime>
+using namespace std;
+
+// Function Declaration
+const string rankCard(), suitCard();
+
+
+int main()
+{
+
+    srand(time(0));
+    for(int i = 0; i < 5; i++)
+    {
+       string card = rankCard()+", "+suitCard();
+       cout << "Karta "<< i+1 <<": (" << card <<")"<< endl;
+    }
+}
+
+const string rankCard() {
+    int rank = (rand() % 13) + 1;
+    switch (rank) {
+    case 1: return "Ac";
+    case 2: return "Dva";
+    case 3: return "Tri";
+    case 4: return "Cetiri";
+    case 5: return "Pet";
+    case 6: return "Sest";
+    case 7: return "Sedum";
+    case 8: return "Osum";
+    case 9: return "Devet";
+    case 10: return "Deset";
+    case 11: return "Dzandar";
+    case 12: return "Dama";
+    case 13: return "Pop";
+    default: string str= to_string(rank + 48);
+    return 0;
+    }
+}
+
+const string suitCard() {
+    int suit = (rand() % 4) + 1;
+    switch (suit) {
+    case 1: return "Lokum";
+    case 2: return "Srce";
+    case 3: return "Detelina";
+    case 4: return "Pik";
+    default: return 0;
+    }
+}
+
+
+
+
+
